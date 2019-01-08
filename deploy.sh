2adfff4aa45071f1b5220a1612f59406cb7f7d9f
Chris Kaknevicius <chris.kaknevicius@Chriss-MacBook-Pro.local>
2019-01-08 09:25:20 -0500

    updates for sha

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t edwolb/multi-client -f ./client/Dockerfile ./client
-docker build -t edwolb/multi-server -f ./server/Dockerfile ./server
-docker build -t edwolb/multi-worker -f ./worker/Dockerfile ./worker
-docker push edwolb/multi-client
-docker push edwolb/multi-server
-docker push edwolb/multi-worker
+docker build -t edwolb/multi-client:latest -t edwolb/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t edwolb/multi-server:latest -t edwolb/multi-client:$SHA -f ./server/Dockerfile ./server
+docker build -t edwolb/multi-worker:latest -t edwolb/multi-client:$SHA -f ./worker/Dockerfile ./worker
+docker push edwolb/multi-client:latest
+docker push edwolb/multi-server:latest
+docker push edwolb/multi-worker:latest
+docker push edwolb/multi-client:$SHA
+docker push edwolb/multi-server:$SHA
+docker push edwolb/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=edwolb/multi-server
+kubectl set image deployments/server-deployment server=edwolb/multi-server:$SHA
+kubectl set image deployments/client-deployment client=edwolb/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=edwolb/multi-worker:$SHA
